cf00525283f82c850c0b71511eb164ef48953e18
nchore <nchoreanthony@gmail.com>
2025-07-30 00:44:55 +0300

    Updated Experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,16 +10,16 @@ const experiences = [
     description: 'Led a team to build scalable data platforms and web apps for financial reporting. Increased product functionality by 35%. Integrated third-party APIs such as Daraja API for mobile payments. Led the API integration project, ensuring smooth implementation and alignment with business goals'
   },
   {
-    role: 'Machine Learning Engineer Intern',
+    role: 'Machine Learning Engineer and Software Engineer',
     company: 'Upwork',
     duration: 'April 2024 – July 2025',
     description: 'Worked on ML models to predict repayment likelihood and reduce default rates. Led a team of 4 developers in designing and developing software applications. Spearheaded the development of a new feature that increased customer engagement and satisfaction. Developed predictive models using machine learning algorithms like linear regression, decision trees, and Random Forests to generate insights from structured datasets. Implemented data preprocessing techniques to clean and prepare datasets for model training. Built end-to-end machine learning pipelines for clients, including: Data Preprocessing: Utilized Pandas, NumPy and scikit-learn to clean and transform datasets . Model Training: Applied scikit-learn for training and fine tuning models.  Deployment: Deployed predictive models using FastAPI, and Docker, ensuring scalability and reliability.'
   },
   
   {
-    role: 'Volunteer Research MAtheatician',
+    role: 'Volunteer Research Mathematician',
     company: 'Aga Khan Foundation',
-    duration: 'Aug 2022 – Oct 2022',
+    duration: 'Aug 2021 – Oct 2021',
     description: 'Analyzed health survey data to inform public health interventions in Kenya.'
   }
 ];
